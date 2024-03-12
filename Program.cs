0186b5daf7144b669a38eb1a1c9a9d04eb6de075
Giovanna <torresssgio@gmail.com>
2024-03-12 16:55:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using Aula02RH.models;
+using Aula02RH.models.Enuns;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Funcionario func = new Funcionario();
+
+        Console.WriteLine("Digite o ID do funcionário:");
+        func.Id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o nome do funcionário:");
+        func.Nome = Console.ReadLine();
+
+        Console.WriteLine("Digite o CPF:");
+        func.Cpf = Console.ReadLine();
+
+        Console.WriteLine("Digite a data de admissão:");
+        func.DataAdmissao = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o salário");
+        func.Salario = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite 1 para Aprendiz e 2 para CLT");
+        int opcao = int.Parse(Console.ReadLine());
+
+        func.TipoFuncionario = (opcao == 1)? TipoFuncionarioEnum.CLT : TipoFuncionarioEnum.Aprendiz;
+
+        func.ReajustarSalario();
+        decimal valorDescontoVT = func.CalcularDescontoVt(6);
+
+        Console.WriteLine("=========================");
+        Console.WriteLine($"O salário reajuste do funcionário é {func.Salario}.\n");
+        Console.WriteLine($"O desconto do VT é {valorDescontoVT}.\n");
+        Console.WriteLine("======================");
+
+    }
+}
---
+++ models/Enuns/TipoFuncionarioEnum.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula02RH.models.Enuns
+{
+    public enum TipoFuncionarioEnum
+    {
+
+        CLT=1,
+
+        Aprendiz=2
+        
+    }
+}
